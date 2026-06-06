55d000adaa01fad6e01ef1f539fd63a83b54845a
Alexander Chaikovsky <sskr33@gmail.com>
2026-06-06 21:15:32 +0300

    test procedural sky

--- res/gamedata/shaders/r3/clouds_volumetric_test.s
+++ res/gamedata/shaders/r3/clouds_volumetric_test.s
@@ -2,6 +2,6 @@ function normal(shader, t_base, t_second, t_detail)
     shader:begin("sky2", "clouds_volumetric_test")
         : fog(false)
         : zb(false, false)
-        : blend(false, blend.one, blend.zero)
+        : blend(true, blend.srcalpha, blend.invsrcalpha)
         : sorting(3, true)
 end
